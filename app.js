4a82eb435cee0c6c4c4659c89c743661da2dbd82
Julian Insua <jii.julious@gmail.com>
2020-12-20 00:03:07 -0300

    Added header and html to the response using write

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const http = require("http");
 
 const rqListener = (req, res) => {
-	console.log(req);
-	process.exit();
+	console.log(req.url, req.method, req.headers);
+	res.setHeader("Content-Type", "text/html");
+	res.write("<html>");
+	res.write("<head><title>My first Node App</title></head>");
+	res.write("<body><h1>Hello World!</h1></body>");
+	res.write("</html>");
+	res.end();
+	//process.exit();
 };
 
 const server = http.createServer(rqListener);
