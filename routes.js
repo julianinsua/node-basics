8495d516abc120fc24bb35618fcb4ae36a46dac8
Julian Insua <jii.julious@gmail.com>
2020-12-26 06:49:35 -0300

    outsourced all functionality to the routes.js file

---
+++ routes.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+
+const requestHandler = (req, res) => {
+	const url = req.url;
+	const method = req.method;
+
+	if (url === "/") {
+		res.setHeader("Content-Type", "text/html");
+		res.write("<html>");
+		res.write("<head><title>Enter Mesage</title></head>");
+		res.write(
+			"<body><form action='/message' method='POST'><input name='message' type='text'/><button type='submit'>Submit</button></form></body>"
+		);
+		res.write("</html>");
+		return res.end();
+	}
+	if (url === "/message" && method === "POST") {
+		const body = [];
+		req.on("data", (chunk) => {
+			console.log(chunk);
+			body.push(chunk);
+		});
+		return req.on("end", () => {
+			const parsedBody = Buffer.concat(body).toString();
+			const message = parsedBody.split("=")[1];
+			fs.writeFile("message.txt", message, () => {
+				res.statusCode = 302;
+				res.setHeader("Location", "/");
+				return res.end();
+			});
+		}); //Using return avoids the code bellow to be excecuted
+	}
+	res.setHeader("Content-Type", "text/html");
+	res.write("<html>");
+	res.write("<head><title>My first Node App</title></head>");
+	res.write("<body><h1>Hello World!</h1></body>");
+	res.write("</html>");
+	res.end();
+	//process.exit();
+};
+
+module.exports = requestHandler;
