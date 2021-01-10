7655ec1bf37c4410a016da6b24327e882e6217b0
Julian Insua <jii.julious@gmail.com>
2021-01-10 15:30:12 -0300

    Added some routing logic using express

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-	console.log("Inside a middleware");
+app.use("/", (req, res, next) => {
+	console.log("Run this every time");
 	next();
 });
 
-app.use((req, res, next) => {
+app.use("/add-product", (req, res, next) => {
+	console.log("Inside the add product middleware");
+	res.send("<h1>Add Product</h1>");
+});
+
+app.use("/", (req, res, next) => {
 	console.log("Inside another middleware");
 	res.send("<h1>Hello from express!</h1>");
 });
