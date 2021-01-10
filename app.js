05303573c4c7f99d01a45dcc86d482206a1b7d31
Julian Insua <jii.julious@gmail.com>
2021-01-10 17:12:32 -0300

    implemented 404 error page and added admin prefix to admin routes

--- app.js
+++ app.js
@@ -9,7 +9,12 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+
+//404 Error page. This should be at the bottom
+app.use((req, res, next) => {
+	res.status(404).send("<h1>Page not found</h1>");
+});
 
 app.listen(3000);
